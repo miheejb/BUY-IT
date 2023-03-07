98fc3c068b0d286246322becfd525d17c6cb64e1
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-03-07 13:13:09 +1300

    <a>connecting from Home jsx to each categorized page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,24 +7,28 @@ const Home = () => {
       title: "Dolls and soft toys",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius interdum euismod. Donec egestas vulputate tincidunt. Sed mollis, sapien id vulputate luctus, elit mi efficitur nis",
       img: "images/doll.png",
+      url: "http://localhost:3000/dollsofttoys",
     },
     {
       id: 2,
       title: "Animal toys",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius interdum euismod. Donec egestas vulputate tincidunt. Sed mollis, sapien id vulputate luctus, elit mi efficitur nis",
       img: "images/animal.png",
+      url: "http://localhost:3000/animaltoys",
     },
     {
       id: 3,
       title: "Cars and trains",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius interdum euismod. Donec egestas vulputate tincidunt. Sed mollis, sapien id vulputate luctus, elit mi efficitur nis",
       img: "images/car-train.png",
+      url: "http://localhost:3000/cartraintoys",
     },
     {
       id: 4,
       title: "LEGOs",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius interdum euismod. Donec egestas vulputate tincidunt. Sed mollis, sapien id vulputate luctus, elit mi efficitur nis",
       img: "images/lego.png",
+      url: "http://localhost:3000/legotoys",
     },
   ];
 
@@ -33,6 +37,7 @@ const Home = () => {
       <div className="posts">
         {posts.map((post) => (
           <div className="post" key={post.id}>
+            <a href={post.url}> 
             <div className="img">
               <img src={post.img} alt="" />
             </div>
@@ -40,6 +45,7 @@ const Home = () => {
               {/* Link???   */}
               <h1>{post.title}</h1>
             </div>
+            </a>
           </div>
         ))}
       </div>
