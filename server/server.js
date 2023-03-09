9929a3e1bd51f5eda4f7ff5b55ff236544783cb6
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-03-09 20:58:57 +1300

    setting up server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose"
+import {DATABASE} from './config.js';
+import authRoutes from './routes/auth.js';
+
+
 
 const app = express();
 
+//db
+mongoose.connect(DATABASE)
+.then(()=> console.log ("db_connected"))
+.catch((err) => console.log(err));
 
 
 //middlewares
@@ -11,11 +20,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-app.get('/api', (req,res)=>{
-    res.json({
-        data:'hello from nodejs api'
-    })
+//routes middleware
+app.use("/api", authRoutes);
 
-});
 
 app.listen(8000, ()=> console.log('server_running_on_port_8000'));
